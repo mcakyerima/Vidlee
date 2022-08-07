f8240510e570b65080c5abe7e0ca2605b1e3b6c4
mcakeyerima <mcakyerima@gmail.com>
2022-08-07 01:29:34 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript : {
     ignoreBuildErrors : true
   },
+  
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"], 
